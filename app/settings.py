4c7452b582f7ccec53f08aa72bbb7d7f21bd2d26
augustojbe <augustojbe>
2025-05-22 21:55:15 -0300

    implementacao das autenticacoes do usuarios viu jwt

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,11 +28,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt',
     
     'genres',
     'actors', 
     'movies',
-    'reviews'
+    'reviews',
+    'authentication',
     
 ]
 
@@ -116,3 +119,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    }
--- genres/views.py
+++ genres/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from genres.models import Genre
 from genres.serializer import GenereSerializer
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
+  permission_classes = (IsAuthenticated,)
   queryset = Genre.objects.all()
   serializer_class  = GenereSerializer
 
 class GenreRetriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = (IsAuthenticated,)
   queryset = Genre.objects.all()
   serializer_class = GenereSerializer
 
