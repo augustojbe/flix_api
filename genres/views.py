c07c7bcd5cca37d8073158a92c9e8d304f2c8eef
augustojbe <augustojbe>
2025-05-23 10:05:20 -0300

    implementacao das permissoes e criacao de grupo no adm do django

--- genres/views.py
+++ genres/views.py
@@ -2,15 +2,16 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from genres.models import Genre
 from genres.serializer import GenereSerializer
+from app.permissions import GlobalDefaultPermission
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
-  permission_classes = (IsAuthenticated,)
+  permission_classes = (IsAuthenticated, GlobalDefaultPermission)
   queryset = Genre.objects.all()
   serializer_class  = GenereSerializer
 
 class GenreRetriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-  permission_classes = (IsAuthenticated,)
+  permission_classes = (IsAuthenticated, GlobalDefaultPermission)
   queryset = Genre.objects.all()
   serializer_class = GenereSerializer
 
